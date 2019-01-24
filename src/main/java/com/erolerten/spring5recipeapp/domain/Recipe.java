54086e489f24514b732d4d3d239969ebd1058340
teodorkasap <erol.erten@gmail.com>
2019-01-24 19:17:39 +0300

    video lecture 150.

--- src/main/java/com/erolerten/spring5recipeapp/domain/Recipe.java
+++ src/main/java/com/erolerten/spring5recipeapp/domain/Recipe.java
@@ -1,6 +1,7 @@
 package com.erolerten.spring5recipeapp.domain;
 
 import javax.persistence.*;
+import java.util.HashSet;
 import java.util.Set;
 
 @Entity
@@ -16,11 +17,12 @@ public class Recipe {
     private Integer servings;
     private String source;
     private String url;
+
+    @Lob
     private String directions;
 
-
     @OneToMany(cascade = CascadeType.ALL, mappedBy = "recipe")
-    private Set<Ingredient> ingredients;
+    private Set<Ingredient> ingredients = new HashSet<>();
 
     @Lob
     private Byte[] image;
@@ -34,7 +36,7 @@ public class Recipe {
     @ManyToMany
     @JoinTable(name = "recipe_category", joinColumns = @JoinColumn(name = "recipe_id"),
             inverseJoinColumns = @JoinColumn(name = "category_id"))
-    private Set<Category> categories;
+    private Set<Category> categories =new HashSet<>();
 
     public Long getId() {
         return id;
@@ -113,11 +115,19 @@ public class Recipe {
     }
 
     public void setNotes(Notes notes) {
+
         this.notes = notes;
+        notes.setRecipe(this);
     }
 
     public Set<Ingredient> getIngredients() {
         return ingredients;
+    }
+
+    public Recipe addIngredient(Ingredient ingredient){
+        ingredient.setRecipe(this);
+        this.ingredients.add(ingredient);
+        return this;
     }
 
     public void setIngredients(Set<Ingredient> ingredients) {
@@ -130,5 +140,13 @@ public class Recipe {
 
     public void setDifficulty(Difficulty difficulty) {
         this.difficulty = difficulty;
+    }
+
+    public Set<Category> getCategories() {
+        return categories;
+    }
+
+    public void setCategories(Set<Category> categories) {
+        this.categories = categories;
     }
 }
--- src/main/java/com/erolerten/spring5recipeapp/repositories/CategoryRepository.java
+++ src/main/java/com/erolerten/spring5recipeapp/repositories/CategoryRepository.java
@@ -3,5 +3,9 @@ package com.erolerten.spring5recipeapp.repositories;
 import com.erolerten.spring5recipeapp.domain.Category;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
+
 public interface CategoryRepository extends CrudRepository<Category, Long> {
+
+    Optional<Category> findByDescription(String description);
 }
