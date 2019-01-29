e8d9ed9af26bfaf82a338f004337d6104f3e4e82
teodorkasap <erol.erten@gmail.com>
2019-01-29 23:27:13 +0300

    Video lecture 200 , without the test class for the RecipeController

---
+++ src/main/java/com/erolerten/spring5recipeapp/controllers/RecipeController.java
@@ -0,0 +1,25 @@
+package com.erolerten.spring5recipeapp.controllers;
+
+import com.erolerten.spring5recipeapp.services.RecipeService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class RecipeController {
+
+    public final RecipeService recipeService;
+
+    public RecipeController(RecipeService recipeService) {
+        this.recipeService = recipeService;
+    }
+
+    @RequestMapping("/recipe/show/{id}")
+    public String showById(@PathVariable String id, Model model){
+
+        model.addAttribute("recipe", recipeService.findById(new Long(id)));
+
+        return "recipe/show";
+    }
+}
